968eebfc7cd5dad7c30a739905ae1d8fb8527d17
Shashank <s_shashank@outlook.com>
2022-02-10 21:21:41 +0530

    updated pom.xml for web

--- pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/springpetclinic/SpringPetClinicApplicationTests.java
@@ -1,6 +1,7 @@
 package springpetclinic;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
