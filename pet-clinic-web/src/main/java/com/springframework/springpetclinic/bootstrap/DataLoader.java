e76511976b2102976533fc5d6e221851fe3f4fad
Shashank <s_shashank@outlook.com>
2022-02-13 15:50:01 +0530

    auto generate map id. closes #23

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/bootstrap/DataLoader.java
@@ -21,14 +21,12 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("John");
         owner1.setLastName("Doe");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Jane");
         owner2.setLastName("Doe");
 
@@ -37,14 +35,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("Loaded Owners...");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("James");
         vet1.setLastName("Webb");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Jim");
         vet2.setLastName("Paul");
 
